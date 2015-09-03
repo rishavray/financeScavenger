8b2324792b1d1b48ccab775216d628c2f50fb5a6
rishavray <rishav.rray@gmail.com>
2015-09-03 10:46:20 +0530

    added fix for no records found

--- getheadlines.py
+++ getheadlines.py
@@ -3,12 +3,14 @@ import urllib2
 from BeautifulSoup import BeautifulSoup
 from datetime import datetime
 from datetime import timedelta
+import re
+import time
 
 #This is the base url. The date is just appened at the end of it
 
 base_url = 'http://archive.financialexpress.com/livearchive/business-news/'
-start_date = '01/03/2008'
-stop_date = '31/03/2008'
+start_date = '01/01/2009'
+stop_date = '30/06/2009'
 
 g = Goose()
 
@@ -17,7 +19,7 @@ stop = datetime.strptime(stop_date, "%d/%m/%Y")
 
 #Generating the archive pages
 archive_pages = []
-
+start_time = time.time()
 while start <= stop:
 	#print start.strftime("%d/%m/%Y") + ' '+ base_url+start.strftime("%d/%m/%Y")
 	archive_pages.append([start.strftime("%d/%m/%Y"),base_url+start.strftime("%d/%m/%Y")])
@@ -28,15 +30,19 @@ for pages in archive_pages:
 	date , link = pages
 	html_page = urllib2.urlopen(link)
 	s = BeautifulSoup(html_page)
+	check = s.findAll(text=re.compile('No records found'),limit=1)
+	if len(check) == 1:
+		continue
 	mydivs = s.findAll("div", { "class" : "archive_tab_list1" })
 	for divs in mydivs:
 		l = BeautifulSoup(str(divs),convertEntities=BeautifulSoup.HTML_ENTITIES)
-		for u in link.findAll('ul'):
+		for u in l.findAll('ul'):
 			cat = (u.li.span.string).strip()
 			for anchor in u.findAll('a'):
-				links.append(anchor.get('href'),cat)
+				links.append([anchor.get('href'),cat])
 
 	for tup in links:
 		url , cat = tup
 		article = g.extract(url=url)
-		print date + ' :: ' + (article.title).encode('UTF-8') + ' :: ' + (article.meta_description).encode('UTF-8') + ' :: ' + cat
+		print date + ' :: ' + (article.title).encode('UTF-8') + ' :: ' + (article.meta_description).encode('UTF-8') + ' :: ' + cat.encode('UTF-8')
+print("--- %s seconds ---" % (time.time() - start_time))
